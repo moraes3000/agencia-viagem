b5b1c0d9e5a42fd8f26e96d028df6742be798084
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-04-23 15:11:31 -0300

    Travel

---
+++ src/components/Travel/index.tsx
@@ -0,0 +1,37 @@
+import { Box, Center, Divider, Grid, GridItem, Heading, Text } from "@chakra-ui/react"
+export default function Travel() {
+    return (
+        <>
+            <Center mt='6rem'>
+                <Grid templateColumns="repeat(5, 1fr)" gap={2} w='960px' textAlign='center' justifyContent='center'>
+                    <GridItem >
+                        <img src="Nightlife.svg" alt="Logo" />
+                    </GridItem>
+                    <GridItem >
+                        <img src="Beach.svg" alt="Logo" />
+                    </GridItem>
+                    <GridItem >
+                        <img src="Modern.svg" alt="Logo" />
+                    </GridItem>
+                    <GridItem >
+                        <img src="Classic.svg" alt="Logo" />
+                    </GridItem>
+                    <GridItem >
+                        <img src="More.svg" alt="Logo" />
+                    </GridItem>
+                </Grid>
+
+
+            </Center>
+            <Center mt='3rem'>
+                <Divider w='200px' h='7px' colorScheme='#47585B' />
+            </Center>
+            <Center mt='3rem'>
+                <Text fontSize="3rem" color='#47585B' >
+                    Vamos nessa?<br/>Então escolha seu continente
+                </Text>
+            </Center>
+
+        </>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import Travel from '../components/Travel'
 export default function Home() {
   return (
     <>
       <Header />
-      <Banner/>
+      <Banner />
+      <Travel />
     </>
   )
 }
