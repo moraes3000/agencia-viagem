bdf0b9ca92b0db2d9266f4b575b411006039a156
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-04-26 15:00:52 -0300

    banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -12,12 +12,12 @@ export default function Banner() {
 
                 <Container maxW="container.lg" color='white'>
                     <Flex>
-                        <Box p='2rem' pr='5rem'>
-                            <Heading fontSize="3rem" color='#F5F8FA'mt='2rem' mb='1rem'>5 Continentes,infinitas possibilidades.</Heading>
-                            <Text fontSize="1.5rem" color='#DADADA' >Chegou a hora de tirar do papel a viagem que você sempre sonhou. </Text>
+                        <Box p={['0.5rem','2rem']} pr='5rem'>
+                            <Heading fontSize={['1.5rem','3rem']} color='#F5F8FA'mt='2rem' mb='1rem'>5 Continentes,infinitas possibilidades.</Heading>
+                            <Text fontSize="['0.5rem,'1.5rem']" color='#DADADA' >Chegou a hora de tirar do papel a viagem que você sempre sonhou. </Text>
                         </Box>
                     
-                        <Box mt='9rem'>
+                        <Box mt='9rem' display={['none','none','block']}>
                             <img src='Airplane.svg' alt='Avião' />
                         </Box>
                     </Flex>
